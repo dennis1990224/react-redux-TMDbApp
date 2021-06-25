a278dd5f824f7b6a9ae1d8efa37995abd2c3003f
dennis1990224 <dennisluzin224@gmail.com>
2021-06-24 23:10:10 -0700

    first commit

---
+++ src/Components/App.js
@@ -0,0 +1,24 @@
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router';
+import * as actionCreators from '../actions/actionCreators';
+import Main from './Main';
+
+const mapStateToProps = (state) => {
+  return {
+    movies: state.movies,
+    hasErrored: state.hasErrored,
+    isLoading: state.isLoading,
+    castDetails: state.castDetails,
+    videoDetails: state.videoDetails,
+    shows: state.shows,
+    currentMovie: state.currentMovie,
+    currentShow: state.currentShow,
+  };
+};
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators(actionCreators, dispatch);
+};
+
+const App = withRouter(connect(mapStateToProps, mapDispatchToProps)(Main));
+export default App;
---
+++ src/Components/MovieDetail.js
@@ -0,0 +1,124 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { cardDetails } from '../Styles/styles';
+import Button from '@material-ui/core/Button';
+import Icon from '@material-ui/core/Icon';
+import YouTube from 'react-youtube';
+import { detailsStyles } from '../Styles/styles';
+import voteAvg from '../Assets/vote-count.svg';
+import releaseDate from '../Assets/release-date.svg';
+import voteCount from '../Assets/vote-avg.svg';
+import CircularProgress from '@material-ui/core/CircularProgress';
+
+export default class MovieDetail extends Component {
+  componentDidMount(){
+    window.scrollTo(0,0);
+    const movieId = this.props.match.params.postId;
+
+    this.props.fetchCurrentMovie(movieId, this.props.movies);
+    this.props.fetchCastData(`https://api.themoviedb.org/3/movie/${movieId}/credits?api_key=28967d69513d49d94603253876b995a8`);
+    this.props.fetchVideo(`https://api.themoviedb.org/3/movie/${movieId}/videos?api_key=28967d69513d49d94603253876b995a8`);
+  }
+  __renderCast(){
+    let renderCastBlock = [];
+    renderCastBlock = this.props.castDetails.cast !== undefined ?
+     this.props.castDetails.cast.map((item, index) => {
+       return (
+        <div key={item.credit_id} className="cast-single">
+          <div className="cast-single__header"
+          style={Object.assign({ backgroundImage:`url(https://image.tmdb.org/t/p/original${item.profile_path})`},cardDetails.castHeader)}>
+            &nbsp;
+          </div>
+          <div className="cast-single__info">
+            <p className="character">{item.character}</p>
+            <h3 className="name">{item.name}</h3>
+          </div>
+        </div>
+       );
+     })
+     : [];
+    return renderCastBlock;
+  }
+  _onReady(event) {
+    // access to player in all event handlers via event.target
+    event.target.playVideo();
+  }
+  render(){
+    if (this.props.hasErrored) {
+      return <div className="movie-listing__error">
+          <h3> Oops! There was an error loading the items </h3>
+      </div>
+    }
+
+    if (this.props.isLoading) {
+      return <div className="movie-listing__error">
+            <CircularProgress size={150} thickness={2}/>
+      </div>;
+    }
+    const opts = {
+      height: '550vh',
+      width: '100%',
+      playerVars: {
+        // https://developers.google.com/youtube/player_parameters
+        autoplay: 0,
+      },
+    };
+    return(
+      <div className="details">
+        {
+          this.props.currentMovie.length > 0 ?
+          <div>
+            <div className="details-header" style={Object.assign({ backgroundImage:`url(https://image.tmdb.org/t/p/original${this.props.currentMovie[0].backdrop_path})`},cardDetails.headerStyles)}>
+              {this.props.videoDetails.results && this.props.videoDetails.results.length > 0 ? <YouTube videoId={this.props.videoDetails.results[0].key} opts={opts} onReady={this._onReady} /> : null}
+              
+            </div>
+            <div className="details-body">
+              <div className="details-content">
+                <div className="details-saver">
+                  <h2>{this.props.currentMovie[0].title}</h2>
+                  {
+                    this.props.currentMovie[0].saved ?
+                    <Button aria-label="Saved" size={'large'} onClick={this.props.toggleDetailSaveCard.bind(null,this.props.currentMovie[0].id)}>
+                      <Icon style={detailsStyles.saveIcon}>
+                          favorite
+                      </Icon>
+                    </Button>
+                    :
+                    <Button aria-label="Save" size={'large'} onClick={this.props.toggleDetailSaveCard.bind(null,this.props.currentMovie[0].id)}>
+                      <Icon style={detailsStyles.saveIcon}>
+                          favorite_border
+                      </Icon>
+                    </Button>
+                  }
+                </div>
+                <div>
+                  <p><strong>Overview :</strong> {this.props.currentMovie[0].overview}</p>
+                </div>
+                <div className="details-additional">
+                  <img src={voteAvg} alt="vote average" />
+                  <p>Vote average: {this.props.currentMovie[0].vote_average} / 10</p>
+                </div>
+                <div className="details-additional">
+                  <img src={voteCount} alt="vote count" />
+                  <p>Vote count: {this.props.currentMovie[0].vote_count}</p>
+                </div>
+                <div className="details-additional">
+                  <img src={releaseDate} alt="release date" />
+                  <p>Release date: {this.props.currentMovie[0].release_date}</p>
+                </div>
+              </div>
+            </div>
+            <div className="details-cast">
+              <p style={cardDetails.castText}>Full Cast</p>
+              {this.__renderCast()}
+            </div>
+          </div>
+          : <p>wait for it</p>
+        }
+      </div>
+    )
+  }
+}
+MovieDetail.propTypes = {
+  movieType: PropTypes.bool,
+};
